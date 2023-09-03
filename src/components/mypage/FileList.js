ccfc5d4a1a54e34c2eecd8385241e81e7d16e9ce
Jeeho Kim <rlawlgh3245@gmail.com>
2023-09-04 06:01:47 +0900

    23/09/04

---
+++ src/components/mypage/FileList.js
@@ -0,0 +1,25 @@
+
+const FileList = ({ fileList, setFileList }) => {
+  const onClick = (name) => {
+    setFileList(fileList.filter((f) => name !== f.name));
+  };
+
+  return (
+    <div>
+      {fileList &&
+        fileList.map((f) => {
+          const src = URL.createObjectURL(f);
+          return (
+            <div key={f.name}>
+              <img src={src} alt="error" width="100" height="100" />
+              <button onClick={() => onClick(f.name)}>X</button>
+            </div>
+          );
+        })}
+
+      
+    </div>
+  );
+};
+
+export default FileList;
--- src/components/mypage/HostRegisterForm.js
+++ src/components/mypage/HostRegisterForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   Form,
   useNavigate,
@@ -7,49 +7,95 @@ import {
   redirect,
 } from "react-router-dom";
 
+import FileList from "./FileList";
+
+const MAX_COUNT = 5;
+
 function HostRegisterForm({ method }) {
   const navigate = useNavigate();
   const navigation = useNavigation();
   const isSubmiting = navigation.state === "submitting";
-
-  const [file, setFile] = useState([]);
+  const ref = useRef(null);
 
   function cancelHandler() {
     navigate("..");
   }
 
-  const onSelectFile = (e) => {
-    if(e.target.files[0]){
-      const [...file] = e.target.files;
-      file.map(f => {
-        setFile((file)=> [...file, {f, fileName : f.name}])
-      })
+  const [fileList, setFileList] = useState([]);
+  // 멀티파일 입력
+  const [fileLimit, setFileLimit] = useState(false);
+  // 파일 개수 제한
+
+  const handleChoose = (e) => {
+    e.preventDefault();
+    ref.current && ref.current.click();
+  };
+
+  const handleChange = (e) => {
+    const uploaded = [...fileList];
+    // 이미 존재하는 파일들의 배열
+    let limitExceeded = false;
+    // 파일제한 개수
+    const files = Array.from(e.target.files);
+    files.some((newFile) => {
+      if (uploaded.findIndex((f) => f.name === newFile.name) === -1) {
+        // 이미 존재하는 파일들의 이름과 새로운 파일의 이름을 비교해서 다른 경우
+        uploaded.push(newFile);
+        // 새로운 파일을 업로드 함
+        if (uploaded.length === MAX_COUNT) {
+          setFileLimit(true);
+          // 파일 제한
+        }
+        if (uploaded.length > MAX_COUNT) {
+          alert("파일제한초과");
+          setFileLimit(false);
+          limitExceeded = true;
+          return true;
+        }
+      }
+    });
+    if (!limitExceeded) {
+      setFileList(uploaded);
     }
   };
 
-  const FileList = ({file, setFile}) => {
-    const onClick = fileName =>{
-      setFile(
-        file.filter(f => f.fileName !== fileName)
-      )
+  const handleUpload = () =>{
+    console.log(fileList)
+    if(!fileList){
+      return;
     }
-    
-    return <div>
-      {
-        file.map(f =>{
-          return <div key={f.fileName}>
-            {f.fileName}
-            <button onClick={()=>onClick(f.fileName)}>X</button>
-          </div>
-        })
-      }
-
-    </div>
+    const data= new FormData();
+    [...fileList].forEach((f)=>{
+      data.append('files', f);
+    })
+    uploadFiles(data);
   }
 
 
+  let url = "http://localhost:8080/api/host/saveImgs";
+
+  const uploadFiles = async data => {
+    Array.from(data).forEach(el=>
+      console.log(el));
+    
+    const response = await fetch(url, {
+      method : 'POST',
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body : data
+    })
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log(err));
+
+    if(!response.ok){
+      throw json({ message: 'Could not save imgs.' }, { status: 500 });
+    }
+  };
+
   return (
-    <Form method={method} file={file}>
+    <Form method={method} encType="multipart/form-data">
       <label htmlFor="shortintro">농장이름</label>
       <input type="text" name="shortintro" id="shortintro" />
       <br />
@@ -99,21 +145,24 @@ function HostRegisterForm({ method }) {
       <br />
       <div>
         <label htmlFor="file">대표이미지</label>
-        <input
-          type="file"
-          name="mainImg"
-        />
+        <input type="file" name="mainImg" />
       </div>
       <div>
-        <label htmlFor="files">
-          이미지
-        </label>
-        <input type="file" name="images" id="uploadFile" multiple  onChange={onSelectFile} />
-        <FileList file={file} setFile={setFile} />
+        <label htmlFor="files">이미지</label>
+        <button onClick={handleChoose}>choose file</button>
+
+        <input
+          type="file"
+          name="images"
+          ref={ref}
+          multiple
+          hidden
+          onChange={handleChange}
+        />
+        <FileList fileList={fileList} setFileList={setFileList} />
+        <button onClick={handleUpload} >파일 업로드</button>
       </div>
-      {file}
-      <input type="hidden" name="files" value={file} />
-      {/* 승인날짜, 승인여부 */}
+
       <div>
         <button type="button" onClick={cancelHandler} disabled={isSubmiting}>
           취소
@@ -123,13 +172,12 @@ function HostRegisterForm({ method }) {
     </Form>
   );
 }
+
 export default HostRegisterForm;
 
 export async function action({ request, params }) {
   const method = request.method;
   const data = await request.formData();
-  const file = data.get("files");
-  console.log(file[0]);
 
   const HostData = {
     shortintro: data.get("shortintro"),
@@ -153,7 +201,6 @@ export async function action({ request, params }) {
   //   formData.append("files", img); // 동일한 key 값으로 계속 담아줌
   // });
 
-
   let url = "http://localhost:8080/api/host/save";
 
   const response = await fetch(url, {
@@ -167,7 +214,7 @@ export async function action({ request, params }) {
   console.log(response);
 
   if (!response.ok) {
-    throw json({ message: 'Could not save board.' }, { status: 500 });
+    throw json({ message: "Could not save board." }, { status: 500 });
   }
-  return redirect('/boards')
+  return redirect("/");
 }
