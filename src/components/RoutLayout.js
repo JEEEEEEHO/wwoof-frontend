074d4e8c3803b8c53e59f50d4cedc0b2b1937389
Jeeho Kim <rlawlgh3245@gmail.com>
2023-12-25 22:42:05 +0900

    23/12/25

--- src/components/RoutLayout.js
+++ src/components/RoutLayout.js
@@ -1,31 +1,31 @@
-import { Outlet, useLoaderData, useNavigation} from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
 import MainNavigation from "./MainNavigation";
 import FooterNavigation from "./FooterNavigation";
 import { useEffect } from "react";
+import WishProvider from "../store/WishProvider";
 
 function RoutLayout() {
   const navigation = useNavigation();
   const token = useLoaderData();
   // const navigation = useNavigation();
-  console.log('RootLayout'+token);
+  console.log("RootLayout" + token);
 
   useEffect(() => {
-    if (token === '') {
+    if (token === "") {
       return;
     }
-
   }, [token]);
-
 
   return (
     <>
-      <MainNavigation />
-      <main>
-        {navigation.state === "loading..." && <p>Loading...</p>}
-        <Outlet></Outlet>
-      </main>
-      <FooterNavigation />
-      
+      <WishProvider>
+        <MainNavigation />
+        <main>
+          {navigation.state === "loading..." && <p>Loading...</p>}
+          <Outlet></Outlet>
+        </main>
+        <FooterNavigation />
+      </WishProvider>
     </>
   );
 }
--- src/store/wish-context.js
+++ src/store/wish-context.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const WishContext = React.createContext({
   items: [],
+  totalAmount: 0,
   addItem: (item) => {},
   removeItem: (id) => {},
   clearWish: () => {}
